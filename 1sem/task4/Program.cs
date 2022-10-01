88ddc25bbaf9929843e09ea01a332c557c0f4455
ilya <ilya_1_3_5@mail.ru>
2022-10-01 17:43:03 +0500

    4 task added

---
+++ 1sem/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("ВВедите значение числа N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.Write("Все четные сила от 1 до N = ");
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
