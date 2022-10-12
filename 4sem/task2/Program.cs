7ed6d68beedd7b816e6a131caf7b78e839378c34
ilya <ilya_1_3_5@mail.ru>
2022-10-12 13:23:18 +0500

    4.2

---
+++ 4sem/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе. Функция должна на вход 
+// принимать число, а выдавать сумму его цифр
+
+
+int enter(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int sum(int N)
+{
+    int number = 0;
+    while (N > 0)
+    {
+        number = number + N % 10;
+        N = N / 10;
+    }
+    return number;
+}
+
+Console.WriteLine(sum(enter("Введите число: ")));
