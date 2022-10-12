4b6792b7878caa997dae19e787d94c512341bda4
ilya <ilya_1_3_5@mail.ru>
2022-10-12 13:15:27 +0500

    4.1

---
+++ 4sem/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B (и в нулевую степень).
+// Использовать свои функции,не использовать Math.Pow
+
+int enter(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int a = enter("Введите первое число: ");
+int b = enter("Введите второе число: ");
+
+int Multy(int a, int b)
+{
+    if(b !=0)
+    {
+        int number = 1;
+    int i = 1;
+    while(i<=b)
+    {
+        number = number * a;
+        i++;
+    }
+    return number;
+    }
+    else
+    {
+        return 1;
+    }
+    
+}
+Console.WriteLine(Multy(a,b));
