88a26a82ecf3700085b7aafc477a1e73b2be0e28
ilya <ilya_1_3_5@mail.ru>
2022-10-19 13:36:44 +0500

    6.1

--- 6sem/1task/Program.cs
+++ 6sem/1task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+string enter(string msg)
+{
+    Console.Write(msg);
+    string number = Console.ReadLine();
+    return number;
+}
+
+string a = " ";
+int count = 0;
+
+while (a != "")
+{
+    a = enter("ВВедите число: ");
+    if (a == "")
+    {
+        break;
+    }
+    int b = Convert.ToInt32(a);
+    if (b > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"{count} положительных числа было введено");
