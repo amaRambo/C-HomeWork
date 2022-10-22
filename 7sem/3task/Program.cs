25a7116e79cc26b217ba1eb27f458f2b32b1dc53
ilya <ilya_1_3_5@mail.ru>
2022-10-22 10:49:48 +0500

    7.3

---
+++ 7sem/3task/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int enter(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverage(int[,] array)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine($"среднеe арифметическое элементов столбца № {j + 1} = {sum / array.GetLength(0)}");
+        sum = 0;
+    }
+
+}
+int m = enter("Введите кол-во строк: ");
+int n = enter("Введите кол-во столбцов: ");
+
+int[,] array = CreateArray(m, n);
+PrintArray(array);
+Console.WriteLine();
+
+FindAverage(array);
