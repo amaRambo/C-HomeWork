5d44ae3c657e7ad592d12def968826ad5255146e
ilya <ilya_1_3_5@mail.ru>
2022-11-13 11:53:12 +0500

    9.1

---
+++ 9sem/2task/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+
+int PrintNumbers(int m, int n)
+{
+    if (m >= n + 1)
+    {
+        return 0;
+    }
+    return m + PrintNumbers(m + 1, n);
+}
+
+int m = Prompt("vvedite 4islo ");
+int n = Prompt("vvedite 4islo ");
+Console.WriteLine();
+Console.WriteLine(PrintNumbers(m, n));
